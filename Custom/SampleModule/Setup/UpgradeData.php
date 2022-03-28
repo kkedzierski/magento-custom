30eeb8acec1c6a685651ee354bd37ff4bbd0a43c
kkedzierski <kamil.kedzierski@onet.com.pl>
2022-03-28 12:36:45 +0200

    add end fill description column to custom_sample_item table

---
+++ Custom/SampleModule/Setup/UpgradeData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Custom\SampleModule\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+
+        if (version_compare($context->getVersion(), '1.0.1', '<')) {
+            $setup->startSetup();
+
+            $setup->getConnection()->update(
+                $setup->getTable('custom_sample_item'),
+                [
+                    'description' => 'Default description'
+                ],
+                $setup->getConnection()->quoteInto('id = ?', 1)
+            );
+
+            $setup->endSetup();
+        }
+
+    }
+}
