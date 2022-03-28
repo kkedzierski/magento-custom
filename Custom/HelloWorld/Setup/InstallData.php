1e2c0c7410cb68b1eb9d1a275593f347b4f80b65
kkedzierski <kamil.kedzierski@onet.com.pl>
2022-03-28 11:50:58 +0200

    change namespace

--- Custom/HelloWorld/Setup/InstallData.php
+++ Custom/HelloWorld/Setup/InstallData.php
@@ -1,8 +1,29 @@
 <?php
 
-namespace Custom\HelloWorld;
+namespace Custom\HelloWorld\Setup;
 
-class InstallData
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
 {
 
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        $setup->getConnection()->insert(
+            $setup->getTable('custom_sample_item'),
+            [
+                'name' => 'Item 1'
+            ]
+        );
+        $setup->getConnection()->insert(
+            $setup->getTable('custom_sample_item'),
+            [
+                'name' => 'Item 2'
+            ]
+        );
+        $setup->endSetup();
+    }
 }
--- Custom/HelloWorld/Setup/InstallSchema.php
+++ Custom/HelloWorld/Setup/InstallSchema.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Custom\HelloWorld;
+namespace Custom\HelloWorld\Setup;
 
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Setup\InstallSchemaInterface;
@@ -15,28 +15,35 @@ class InstallSchema implements InstallSchemaInterface
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-
         try {
-            $table = $setup->getConnection()->newTable(
-                $setup->getTable('custom_sample_item')
-            )->addColumn(
-                'id',
-                Table::TYPE_INTEGER,
-                null,
-                ['identity' => true, 'nullable' => false, 'primary' => true],
-                'Item id'
-            )->addColumn(
-                'name',
-                Table::TYPE_TEXT,
-                255,
-                ['nullable' => false],
-                'Item name'
-            )->addIndex(
-                $setup->getIdxName('custom_sample_item', ['name']),
-                ['name']
-            )->setComment(
-                'Sample Items'
-            );
+            $table = $setup->getConnection()
+                ->newTable($setup->getTable('custom_sample_item'))
+                ->addColumn(
+                    'id',
+                    Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'nullable' => false,
+                        'primary' => true,
+                        'unsigned' => true
+                    ],
+                    'Item id'
+                )
+                ->addColumn(
+                    'name',
+                    Table::TYPE_TEXT,
+                    255,
+                    ['nullable' => false],
+                    'Item name'
+                )
+                ->addIndex(
+                    $setup->getIdxName('custom_sample_item', ['name']),
+                    ['name']
+                )
+                ->setComment(
+                    'Sample Items'
+                );
             $setup->getConnection()->createTable($table);
         } catch (Zend_Db_Exception $e) {
             $level = 'ERROR';
