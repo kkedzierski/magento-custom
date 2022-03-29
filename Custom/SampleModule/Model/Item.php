28425892f0d37eb5a374b94728fb1df54153eed2
kkedzierski <kamil.kedzierski@onet.com.pl>
2022-03-29 13:43:36 +0200

    add custom:item:add command

--- Custom/SampleModule/Model/Item.php
+++ Custom/SampleModule/Model/Item.php
@@ -2,12 +2,24 @@
 
 namespace Custom\SampleModule\Model;
 
+use Custom\SampleModule\Model\ResourceModel\Item as ItemModel;
 use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Data\Collection\AbstractDb;
 
 class Item extends AbstractModel
 {
-    public function __construct()
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = [])
     {
-        $this->_init(\Custom\SampleModule\Model\ResourceModel\Item::class);
+        $this->_init(ItemModel::class);
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
     }
+
 }
