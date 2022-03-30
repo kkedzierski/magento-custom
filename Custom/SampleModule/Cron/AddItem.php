837a4da871cc96621497d667ac6e2e9bb895f821
kkedzierski <kamil.kedzierski@onet.com.pl>
2022-03-30 16:56:04 +0200

    create admin store configuration panel to change shedule of add items

--- Custom/SampleModule/Cron/AddItem.php
+++ Custom/SampleModule/Cron/AddItem.php
@@ -3,22 +3,27 @@
 namespace Custom\SampleModule\Cron;
 
 use Custom\SampleModule\Model\ItemFactory;
+use Custom\SampleModule\Model\Config;
 
 class AddItem
 {
     private ItemFactory $itemFactory;
+    private Config $config;
 
-    public function __construct(ItemFactory $itemFactory)
+    public function __construct(ItemFactory $itemFactory, Config $config)
     {
         $this->itemFactory = $itemFactory;
+        $this->config = $config;
     }
 
     public function execute()
     {
-        $this->itemFactory->create()
-            ->setName('Scheduled item')
-            ->setDescription('created at '. time())
-            ->save();
+        if( $this->config->isEnabled() ){
+            $this->itemFactory->create()
+                ->setName('Scheduled item')
+                ->setDescription('created at '. time())
+                ->save();
+        }
     }
 
 }
---
+++ Custom/SampleModule/Model/config.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Custom\SampleModule\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class config
+{
+    const XML_PATH_ENABLED = 'custom/general/enabled';
+//    const XML_PATH_CRON_EXPRESSION = 'custom/general/cron_expression';
+
+    private ScopeConfigInterface $config;
+
+    public function __construct(ScopeConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    public function isEnabled()
+    {
+        return $this->config->getValue(self::XML_PATH_ENABLED);
+    }
+}
